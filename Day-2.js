2e92e89329a2892c21b356b650769ee43356dab1
Sujit kumar <sujittkumar11@gmail.com>
2024-08-30 00:34:45 +0530

    day two commit

---
+++ Day-2.js
@@ -0,0 +1,56 @@
+// Activity 1: Arithmetic Operations
+// Task 1: Write a program to add two numbers and log the result to the console.
+// Task 2: Write a program to subtract two numbers and log the result to the console.
+// Task 3: Write a program to multiply two numbers and log the result to the console.
+// Task 4: Write a program to divide two numbers and log the result to the console.
+// Task 5: Write a program to find the remainder when one number is divided by another and log the result to the console.
+let num1 = 80;
+let num2 =68;
+console.log(`addtion is ${num1+num2}`);
+console.log(`subtraction is ${num1-num2}`);
+console.log(`multipcation is ${num1*num2}`);
+console.log(`division is ${num1/num2}`);
+console.log(`the remender is ${num1%num2}`);
+
+
+
+// Activity 2: Assignment Operators
+// Task 6: Use the += operator to add a number to a variable and log the result to the console.
+let x= 55;
+x+=5 //so now x is 60;
+console.log(x);
+
+// Task 7: Use the -= operator to subtract a number from a variable and log the result to the console.
+x-=10
+console.log(x);
+
+// Activity 3: Comparison Operators
+// Task 8: Write a program to compare two numbers using > and < and log the result to the console.
+let age = 23;
+console.log(`age is greater than 18 : ${age>18}`);
+console.log(`age is smaller than 18 :${age<18}`);
+
+// Task 9: Write a program to compare two numbers using >= and <= and log the result to the console.
+console.log(5>=5);
+console.log(8<=10);
+
+// Task 10: Write a program to compare two numbers using == and === and log the result to the console.
+console.log(5=="5");//true
+console.log(5==="5");//false
+
+// Activity 4: Logical Operators
+// Task 11: Write a program that uses the && operator to combine two conditions and log the result to the console.
+console.log(true && false);
+
+// Task 12: Write a program that uses the || operator to combine two conditions and log the result to the console.
+console.log(true || false);
+
+// Task 13: Write a program that uses the ! operator to negate a condition and log the result to the console.
+let i=0;
+console.log(i!=1);
+
+// Activity 5: Ternary Operator
+// Task 14: Write a program that uses the ternary operator to check if a number is positive or negative and log the result to the console.
+let k = -44;
+console.log(k>0?"its positive":"negative");
+
