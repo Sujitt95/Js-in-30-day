feaa2de4cbdf6e0a4b2c13a96c8e49342b5e2213
Sujit kumar <sujittkumar11@gmail.com>
2024-09-04 00:34:53 +0530

    day 4 commit

---
+++ Day-4.js
@@ -0,0 +1,75 @@
+// Activity 1: For Loop
+// Task 1: Write a program to print numbers from 1 to 10 using a for loop.
+for(let i=1;i<=10;i++){
+    console.log(i); 
+}
+
+// Task 2: Write a program to print the multiplication table of 5 using a for loop.
+let x =5;
+for(let i=1;i<=10;i++){
+    console.log(`${x} * ${i} = ${i*x}`);
+    
+}
+
+// Activity 2: While Loop
+// Task 3: Write a program to calculate the sum of numbers from 1 to 10 using a while loop.
+let sum =0;
+let i=1;
+while (i<=10) {
+    sum+=i;
+    i++
+}
+console.log(sum);
+
+// Task 4: Write a program to print numbers from 10 to 1 using a while loop.
+i= 10
+while (i>0) {
+    console.log(i);
+    i--
+}
+
+// Activity 3: Do...While Loop
+// Task 5: Write a program to print numbers from 1 to 5 using a do...while loop.
+let k=1;
+do{
+    console.log(k);
+    k++;
+}while(k<=5);
+
+// Task 6: Write a program to calculate the factorial of a number using a do...while loop.
+let ft=1;
+let l=1
+do{
+    ft*=l;
+    l++;
+}while(l<=5)
+    console.log("the factorial is ",ft);
+    
+// Activity 4: Nested Loops
+// Task 7: Write a program to print a pattern using nested for loops:
+// *
+// * *
+// * * *
+// * * * *
+// * * * * *
+for(let i=1;i<=5;i++){
+    let ch=""
+    for(let j=1;j<=i;j++){
+        ch+="* "
+    }
+    console.log(ch);
+}
+
+// Activity 5: Loop Control Statements
+// Task 8: Write a program to print numbers from 1 to 10, but skip the number 5 using the continue statement.
+for(let i=1;i<=10;i++){
+    if(i===5) continue;
+    console.log(i);
+    
+}
+
+// Task 9: Write a program to print numbers from 1 to 10, but stop the loop when the number is 7 using the break statement.
+for(let i=1;i<=10;i++){
+    if(i==8) break;
+    console.log(i);
+}
